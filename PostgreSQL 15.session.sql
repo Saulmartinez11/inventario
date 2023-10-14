570f83a3f6b065528fed97183e978652d300911f
Saul Martinez <72671687+Saulmartinez11@users.noreply.github.com>
2023-10-14 02:40:48 -0600

    Add files via upload

---
+++ PostgreSQL 15.session.sql
@@ -0,0 +1,10 @@
+-- Active: 1689740799344@@localhost@5432@ProyectoGraduacion
+
+SELECT id_usuario, nombre, cargo, "contraseña"
+	FROM public.usuarios;
+
+
+SELECT cod_categoria, categoria
+	FROM public.categoria;
+
+	ROLLBACK
